5a6860a70f4252ce1ad2e8c7462f04d0c4921907
Jamie Sefton <jamie.sefton@parall.ax>
2017-10-11 13:41:15 +0100

    Updated to try fix namespace issue

--- src/Push.php
+++ src/Push.php
@@ -3,5 +3,30 @@ namespace Jsefton\WebPush;
 
 class Push
 {
-    
+    // Not going to be used just yet...
+    protected $version = '1.0';
+
+    /**
+     * @var string
+     */
+    protected $mode = 'sandbox';
+
+    /**
+     * @var string
+     */
+    protected $apiDomain = "http://web-push.dev";
+    protected $sandboxApiDomain = "http://web-push.dev";
+
+    /**
+     * @var array
+     */
+    protected $endpoints = [
+        'subscribe' => '/subscribe',
+        'notify' => '/notify'
+    ];
+
+    public static function send($opts)
+    {
+        pr($opts);
+    }
 }
